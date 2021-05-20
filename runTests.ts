40b48673f1d23d085c197e80f78e0cf2830fadd2
Joe Langlois <joe.desruisseaux.langlois@gmail.com>
2021-05-20 06:42:35 -0400

    initial commit

---
+++ runTests.ts
@@ -0,0 +1,6 @@
+import {test} from 'uvu'
+
+import './src/main_test'
+
+
+test.run()
---
+++ src/main_test.ts
@@ -0,0 +1,6 @@
+import {test} from 'uvu'
+import * as assert from 'uvu/assert'
+
+test('equality', () => {
+    assert.equal(1 + 1, 2)
+})
